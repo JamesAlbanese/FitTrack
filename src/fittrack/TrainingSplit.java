f121762130bcde5bd168378236dfb4b90ecd72ec
James Albanese <j.albanese2003@gmail.com>
2026-04-16 18:20:29 -0400

    Add Training Split enums

--- src/fittrack/TrainingSplit.java
+++ src/fittrack/TrainingSplit.java
@@ -2,4 +2,16 @@ package fittrack;
 
 public enum TrainingSplit {
 
+    PPL("PPL (Push / Pull / Legs)", SplitDay.PUSH, SplitDay.PULL, SplitDay.LEGS),
+
+    UL("UL (Upper / Lower)", SplitDay.UPPER, SplitDay.LOWER),
+
+    FBEOD("FBEOD (Full Body Every Other Day)", SplitDay.FULL_BODY),
+
+    ARNOLD("Arnold Split (Chest & Back / Shoulders & Arms / Legs", SplitDay.CHEST_BACK, SplitDay.SHOULDERS_ARMS, SplitDay.LEGS),
+
+    ANTERIOR_POSTERIOR("Anterior / Posterior", SplitDay.ANTERIOR, SplitDay.POSTERIOR),
+
+    PPLxUL("PPLxUL (Push / Pull / Legs / Upper / Lower)", SplitDay.PUSH, SplitDay.PULL, SplitDay.LEGS, SplitDay.UPPER, SplitDay.LOWER);
+
 }
