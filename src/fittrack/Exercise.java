ca3565929cff8bce96d3d633fb2b5c3e02834f6b
James Albanese <j.albanese2003@gmail.com>
2026-04-16 19:57:47 -0400

    Added instance variables and constructor to Exercise

--- src/fittrack/Exercise.java
+++ src/fittrack/Exercise.java
@@ -1,4 +1,30 @@
 package fittrack;
 
-public class Exercise {
+import java.io.Serializable;
+
+public class Exercise implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String name;
+
+    private int sets;
+
+    private int reps;
+
+    private double weight;
+
+    //Optional notes about exercise
+    private String notes;
+
+    public Exercise(String name, int sets, int reps, double weight, String notes){
+        this.name = name;
+        this.sets = sets;
+        this.reps = reps;
+        this.weight = weight;
+        if(notes == null){
+            this.notes = "";
+        }
+        this.notes = notes;
+    }
 }
