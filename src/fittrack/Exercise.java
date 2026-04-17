cf861945d349737b1cfbcd3b7107b3dd27a32fb5
James Albanese <j.albanese2003@gmail.com>
2026-04-16 20:16:05 -0400

    Added helper methods, a create exercise function, and toString that isn't finished yet

--- src/fittrack/Exercise.java
+++ src/fittrack/Exercise.java
@@ -17,14 +17,68 @@ public class Exercise implements Serializable {
     //Optional notes about exercise
     private String notes;
 
-    public Exercise(String name, int sets, int reps, double weight, String notes){
+    public Exercise(String name, int sets, int reps, double weight){
         this.name = name;
         this.sets = sets;
         this.reps = reps;
         this.weight = weight;
-        if(notes == null){
-            this.notes = "";
-        }
+        this.notes = "";
+
+    }
+
+
+    public static Exercise create(String name, int sets, int reps){
+        // I'm gonna create a custom Exception class so that if the
+        // arguments passed to exercise are invalid it will catch and
+        //it can be handled somehow
+
+        return new Exercise(name, sets, reps, 0);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSets() {
+        return sets;
+    }
+
+    public void setSets(int sets) {
+        this.sets = sets;
+    }
+
+    public int getReps() {
+        return reps;
+    }
+
+    public void setReps(int reps) {
+        this.reps = reps;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
         this.notes = notes;
+    }
+
+    @Override
+    public String toString(){
+        //some stringbuilder stuff to nicely format the actual exercise
+        return "";
+        
     }
 }
