c1c5313a011cfdfd280cb92637f837f6bbf96a33
James Albanese <j.albanese2003@gmail.com>
2026-05-02 16:17:39 -0400

    Add getters, setters, and toString

--- src/fittrack/ExerciseSet.java
+++ src/fittrack/ExerciseSet.java
@@ -26,4 +26,39 @@ public class ExerciseSet implements Serializable{
         this.weight = weight;
     }
 
+    public int getReps(){
+        return reps;
+    }
+
+    public void setReps(int reps){
+        if(reps <= 0){
+            //custom exception
+        }
+
+        this.reps = reps;
+    }
+
+
+    public double getWeight(){
+        return weight;
+    }
+
+
+    public void setWeight(double weight){
+        if(weight < 0){
+            //custom exception
+        }
+        this.weight = weight;
+    }
+
+
+    @Override
+    public String toString(){
+        //Zero weight usually means a bodyweight exercise
+        if(weight > 0){
+            return reps + " reps @ " + weight + " lbs";
+        }
+        return reps + " reps (bodyweight)";
+    }
+
 }
