aa2557571d2555ee794071b0330c0236360dc9e4
James Albanese <j.albanese2003@gmail.com>
2026-05-03 18:56:12 -0400

    started filehandler class to write temp workouts as binary data, with constructor

---
+++ src/fittrack/FileHandler.java
@@ -0,0 +1,13 @@
+package fittrack;
+
+
+//File handler class handles writing temporary workout sessions to be saved later
+public class FileHandler {
+
+    private static final String WORKOUT_BINARY = "fittrack_data.dat";
+
+    //prevents instantiation of filehandler objects
+    private FileHandler(){}
+
+    
+}
