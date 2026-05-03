eac4b3c359430c645ebff62666d61d311be1b71e
James Albanese <j.albanese2003@gmail.com>
2026-05-03 17:02:48 -0400

    Added WorkoutSession attributes and its constructor

---
+++ src/fittrack/WorkoutSession.java
@@ -0,0 +1,39 @@
+package fittrack;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WorkoutSession implements Serializable {
+
+    private static final long serialVersioUID = 1L;
+
+    private LocalDate date;
+    private int durationMins;
+    private TrainingSplit split;
+    private SplitDay splitDay;
+    private List<Exercise> exercises;
+    private CardioSession cardio;
+    private String notes;
+
+    public WorkoutSession(LocalDate date, int durationMins, TrainingSplit split, SplitDay splitDay){
+        if(date == null){
+            //custom exception
+        }
+        if(durationMins <= 0){
+            //custom exception
+        }
+        if(!split.containsDay(splitDay)){
+            //custom exception
+        }
+        this.date = date;
+        this.durationMins = durationMins;
+        this.split = split;
+        this.splitDay = splitDay;
+        this.exercises = new ArrayList<>();
+        this.cardio = null;
+        this.notes = "";
+
+    }
+}
