3d5cfa6728538f6d5dec77f85cac44a2d7c252fb
James Albanese <j.albanese2003@gmail.com>
2026-05-03 21:33:09 -0400

    Created save and load session methods

--- src/fittrack/FileHandler.java
+++ src/fittrack/FileHandler.java
@@ -1,6 +1,10 @@
 package fittrack;
 
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 //File handler class handles writing temporary workout sessions to be saved later
 public class FileHandler {
 
@@ -9,5 +13,41 @@ public class FileHandler {
     //prevents instantiation of filehandler objects
     private FileHandler(){}
 
-    
+    public static void saveSessions(List<WorkoutSession> sessions) throws IOException {
+        try(FileOutputStream FOS = new FileOutputStream(WORKOUT_BINARY);
+            ObjectOutputStream OOS = new ObjectOutputStream(FOS)){
+
+            for(WorkoutSession session : sessions){
+                OOS.writeObject(session);
+            }
+        }
+    }
+
+
+    public static List<WorkoutSession> loadSessions() throws IOException, ClassNotFoundException{
+
+        List<WorkoutSession> sessions = new ArrayList<>();
+        File file = new File(WORKOUT_BINARY);
+
+        if(!file.exists()){
+            return sessions;
+        }
+
+        try(FileInputStream FIS = new FileInputStream(file);
+            ObjectInputStream OIS = new ObjectInputStream(FIS)){
+
+            while(true){
+                try{
+                    WorkoutSession session = (WorkoutSession) OIS.readObject();
+                    sessions.add(session);
+                }catch(EOFException e){
+                    break;
+                }
+            }
+        }finally{
+            System.out.println("Session load complete. Total loaded: "+ sessions.size());
+        }
+
+        return sessions;
+    }
 }
