00d2ba3ce570c24ce4f4a9844a52f2d37ebd7b7f
James Albanese <j.albanese2003@gmail.com>
2026-05-04 21:13:52 -0400

    setup FitrackApp attributes, FitTrackApp is entry point of program

--- src/fittrack/FitTrackApp.java
+++ src/fittrack/FitTrackApp.java
@@ -1,8 +1,27 @@
 package fittrack;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class FitTrackApp extends JFrame {
 
     private static final String TITLE = "FitTrack";
+
+    private static final int APP_WIDTH = 900;
+    private static final int APP_HEIGHT = 600;
+
+
+    private final FitTrackManager manager;
+
+    private final MainPanel mainPanel;
+
+    private final LogWorkoutPanel logWorkoutPanel;
+
+    private final CardLayout cardLayout;
+
+    private final JPanel container;
+
+
+    private static final String MAIN_PANEL = "MAIN";
+    private static final String LOG_WORKOUT_PANEL = "LOG_WORKOUT";
 }
