ad5135706daa6921c36e2e2eeb9eb136e088aeea
James Albanese <j.albanese2003@gmail.com>
2026-05-04 21:33:58 -0400

    setup numerous logworkoutpanel attributes

--- src/fittrack/LogWorkoutPanel.java
+++ src/fittrack/LogWorkoutPanel.java
@@ -1,6 +1,35 @@
 package fittrack;
 
-public class LogWorkoutPanel {
+import javax.swing.*;
+
+public class LogWorkoutPanel extends JPanel {
+
+    //app and manager
+    private final FitTrackApp app;
+    private final FitTrackManager manager;
+
+
+    //session info
+    private JComboBox<TrainingSplit> splitCombo;
+    private JComboBox<SplitDay> dayCombo;
+    private JTextField dateField;
+    private JTextField durationField;
+    private JTextField notesField;
+
+
+    //exercise builder fields
+    private JTextField exerciseNameField;
+    private JTextField setRepsField;
+    private JTextField setWeightField;
+    private JTextField exerciseNotesField;
+    private JButton addSetButton;
+    private JButton finalizeButton;
+    private JPanel stagedSetsPanel;
+    private JLabel stagedSetsLabel;
+    private JPanel exerciseListPanel;
+    private JLabel exerciseCountLabel;
+
+
 
 
 }
